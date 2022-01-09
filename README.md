059b90b71fd6b4ad6cf98158f33dd4bbb1ced66f
GerhardAhrens <gerhard.ahrens@lifeprojects.de>
2022-01-09 08:42:49 +0100

    Projektdateien hinzufügen.

---
+++ README.md
@@ -0,0 +1,6 @@
+# TryConvert
+<img src="./TryConvert.jfif" style="width:100px;"/>
+
+
+## The program is written for .NET5
+
---
+++ TryConvert/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace TryConvertDemo
+{
+    using System;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
